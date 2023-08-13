ca8453e2a4356f4795ee8068792f871ba2dbc0e6
TheBlackWolf88 <mopszi88@gmail.com>
2023-08-14 00:29:18 +0200

    mostmar tenyleg done a day3 (forgor part 2)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -30,17 +30,57 @@ let mut sum = 0;
         }).collect();
         
         let a = 1;
-        let A = 27;
-
-        println!("{:?}",'a' as usize);
-        println!("{:?}",'A' as usize);
+        let big_a = 27;
         
         if *common_key[0] as usize > 96 {
             sum += *common_key[0] as usize - 'a' as usize + a;
         } else {
-            sum += *common_key[0] as usize - 'A' as usize + A;
+            sum += *common_key[0] as usize - 'A' as usize + big_a;
         }
    }
 
-    println!("{:?}", sum);
+    println!("Part 1: {:?}", sum);
+
+    let file2 = File::open("input.txt");
+    let reader2 = BufReader::new(file2.unwrap());
+    let mut buffer: Vec<String> = Vec::with_capacity(3);
+    let mut sum2 = 0;
+    for item in reader2.lines() {
+        buffer.push(item.unwrap());
+        if buffer.len() == 3 {
+            let mut map3: HashMap<char, usize> = HashMap::new();
+            let mut map4: HashMap<char, usize> = HashMap::new();
+            let mut map5: HashMap<char, usize> = HashMap::new();
+            buffer[0].chars().for_each(|c| {
+                *map3.entry(c).or_insert_with(|| 1) += 1;
+            });
+            buffer[1].chars().for_each(|c| {
+                *map4.entry(c).or_insert_with(|| 1) += 1;
+            });
+            buffer[2].chars().for_each(|c| {
+                *map5.entry(c).or_insert_with(|| 1) += 1;
+            });
+            let badge:Vec<&char> = map3.keys().filter_map(|k| {
+                if map4.contains_key(k) && map5.contains_key(k) {
+                    Some(k)
+                }
+                else {
+                    None
+                }
+
+            }).collect();
+
+            let a = 1;
+            let big_a = 27;
+        
+            if *badge[0] as usize > 96 {
+                sum2 += *badge[0] as usize - 'a' as usize + a;
+            } else {
+                sum2 += *badge[0] as usize - 'A' as usize + big_a;
+            }
+            buffer.clear();
+        }
+    }
+
+    println!("Part 2: {:?}", sum2);
 }
